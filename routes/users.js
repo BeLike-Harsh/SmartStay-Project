d621c2c7933582c9794ee1acd96af7ed71c8890e
Harsh Kumar <harshkrs2004@gmail.com>
2025-02-18 21:50:07 +0530

    add user signup and login functionality with views

--- routes/users.js
+++ routes/users.js
@@ -2,9 +2,41 @@ const express=require('express');
 const router=express.Router();
 const wrapAsync=require("../utils/wrapAsync.js");
 const ExpressError=require("../utils/ExpressError.js");
+const User=require("../models/user.js");
+const passport=require('passport')
+
 
 router.get("/signup",(req,res) => {
     res.render("./user/signup.ejs");
 })
+
+
+router.post("/signup",wrapAsync(async(req,res) => {
+    try{
+        let {username,email,password}=req.body;
+        const newUser=new User({username,email});
+        let usernew=await User.register(newUser,password);
+        req.flash("success","you registered successfully");
+        console.log(usernew);
+        res.redirect("/lists");
+    }catch(e){
+        req.flash("error",e.message);
+        res.redirect("/signup");
+    }
+   
+}))
+
+//Login Page
+
+router.get("/login",(req,res) => {
+    res.render("./user/login.ejs")
+})
+
+router.post("/login",passport.authenticate("local",{failureRedirect:"/login",failureFlash:true}),
+     async(req,res) => {
+        res.flash("success","welcome User");
+        res.redirect("/lists");
+     }
+)
 
 module.exports=router;
